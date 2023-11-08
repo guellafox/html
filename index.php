97b2687ef382e0f504e8fffcee1e7001fef8084e
unknown <APRENDIZ@MEDAPRCSGFSP475.FormacionSena.RED>
2023-11-08 12:31:53 -0500

    trabajo completo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de cursos</title>
+</head>
+<body>
+    <h1>Formulario de Cursos</h1>
+    <form action="enviar.php" method="post">
+        nombre: <input type="text" name="codigo"><br>
+        apellido: <input type="text" name="nombre"><br>
+        correo electronico: <input type="text" name="documento_id"><br>
+        <input type="submit" value="Guardar">
+    </form>
+</body>
+</html>
